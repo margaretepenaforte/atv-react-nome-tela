5e5e57098a60aeb035879670fdfd0428879d6ff8
MARGARETE PENAFORTE <109976418+margaretepenaforte@users.noreply.github.com>
2022-11-01 08:58:41 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-import firsComponents from './components/firstComponents';
+import FirsComponent from './components/FirstComponent';
+import AnotherComponent from './components/AnotherComponent';
+import Images from './components/Images';
+import Hooks from './components/Hooks';
+import Fragment from './components/Fragment';
+import Container from './components/Container';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <firsComponents />
+      <FirsComponent />
+      <AnotherComponent />
+      <Images />
+      <Hooks />
+      <Fragment />
+      <Container>
+        <h1>Este é filho container!</h1>
+      </Container>
     </div>
   );
 }
