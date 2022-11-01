740f30a6445137b96e999ceef4fde3da9a46abea
margaretepenaforte <penafortemargarete@gmail.com>
2022-11-01 08:33:39 -0300

    primeiro commit

---
+++ src/components/firstComponents.js
@@ -0,0 +1,13 @@
+function firsComponents(){
+    // comentários
+
+    return(
+        <div className="firstcomponent">
+            {/* comentários no jsx */}
+            <p>Primeiro Componente</p>
+            {2 + 2}
+        </div>
+    )
+}
+
+export default firsComponents
